c9636dea1f858c93a4e148d3d31b033542bf7e44
Ju, Seonghwan <crtevn@gmail.com>
2025-11-04 20:02:44 +0900

    update

--- supplier_data.js
+++ supplier_data.js
@@ -1,75 +1,91 @@
-const gnfs_1 = data.filter((item) =>
+function makeSupplierData(row_data, items) {
+  return items
+    .map((id) => row_data.find((item) => item.id === id))
+    .filter(Boolean);
+}
+
+const gnfs_1 = makeSupplierData(
+  data,
   [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 11, 13, 205, 44, 45, 46, 47, 20, 48, 49,
     50, 161, 51, 52, 22, 53, 29, 54, 55, 99, 98, 57, 56,
-  ].includes(item.id)
+  ]
 );
 
-const gnfs_2 = data.filter((item) =>
-  [100, 103, 102, 101, 95, 17, 96, 97].includes(item.id)
-);
+const gnfs_2 = makeSupplierData(data, [100, 103, 102, 101, 95, 17, 96, 97]);
 
-const gnfs_3 = data.filter((item) =>
+const gnfs_3 = makeSupplierData(
+  data,
   [
     31, 59, 60, 61, 30, 204, 36, 63, 33, 64, 65, 66, 67, 68, 69, 62, 21, 70, 71,
     34, 72, 73, 75, 76, 74,
-  ].includes(item.id)
+  ]
 );
 
-const gnfs_4 = data.filter((item) =>
+const gnfs_4 = makeSupplierData(
+  data,
   [
     37, 77, 42, 35, 38, 39, 40, 41, 58, 78, 79, 80, 81, 82, 83, 43, 84, 85, 86,
     87, 88, 90, 93, 91, 92, 89, 94, 18, 32,
-  ].includes(item.id)
+  ]
 );
 
-const gnfs_5 = data.filter((item) =>
-  [165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177].includes(
-    item.id
-  )
+const gnfs_5 = makeSupplierData(
+  data,
+  [165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177]
 );
 
-const gnfs_6 = data.filter((item) =>
-  [178, 179, 180, 181, 182, 183, 184, 185, 186].includes(item.id)
+const gnfs_6 = makeSupplierData(
+  data,
+  [178, 179, 180, 181, 182, 183, 184, 185, 186]
 );
 
-const well_1 = data.filter((item) =>
-  [
-    187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 114, 115, 116, 117, 118,
-  ].includes(item.id)
+const well_1 = makeSupplierData(
+  data,
+  [187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 114, 115, 116, 117, 118]
 );
 
-const well_2 = data.filter((item) =>
+const well_2 = makeSupplierData(
+  data,
   [
     119, 120, 121, 122, 123, 124, 128, 125, 126, 127, 129, 130, 131, 132, 133,
     134, 135, 136, 137, 138, 139, 140,
-  ].includes(item.id)
+  ]
 );
 
-const noori = data.filter((item) => [150, 151].includes(item.id));
-const sunin = data.filter((item) =>
-  [141, 142, 143, 144, 145, 146, 147, 148, 149, 197].includes(item.id)
+const noori = makeSupplierData(data, [150, 151]);
+
+const sunin = makeSupplierData(
+  data,
+  [141, 142, 143, 144, 145, 146, 147, 148, 149, 197]
 );
-const hosu = data.filter((item) => [155, 156, 157, 158].includes(item.id));
-const boram = data.filter((item) => [164].includes(item.id));
-const alice = data.filter((item) => [161, 162, 163].includes(item.id));
-const mirstar = data.filter((item) => [159, 160].includes(item.id));
-const marubishi = data.filter((item) => [152, 153, 154].includes(item.id));
-const cannele = data.filter((item) => [199].includes(item.id));
-const eggtart = data.filter((item) => [200].includes(item.id));
+
+const hosu = makeSupplierData(data, [155, 156, 157, 158]);
+
+const boram = makeSupplierData(data, [164]);
+
+const alice = makeSupplierData(data, [161, 162, 163]);
+
+const mirstar = makeSupplierData(data, [159, 160]);
+
+const marubishi = makeSupplierData(data, [152, 153, 154]);
+
+const cannele = makeSupplierData(data, [199]);
+
+const eggtart = makeSupplierData(data, [200]);
 
 function renderTableRows(dataArray, tbodyElement) {
-    dataArray.forEach((item) => {
-      const tr = document.createElement("tr");
-  
-      tr.innerHTML = `
+  dataArray.forEach((item) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
         <td>${item.id}</td>
         <td>${item.item_name}</td>
         <td>${item.min_order_quantity}</td>
         <td>${item.order_rule}</td>
         <td>${item.location}</td>
       `;
-  
-      tbodyElement.appendChild(tr);
-    });
-  }
+
+    tbodyElement.appendChild(tr);
+  });
+}
